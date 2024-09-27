c01a5c95d109efd8a4523d50e2188e3dd461a10c
Adrian Rios Ramos <adrian150604@gmail.com>
2024-09-26 21:24:36 -0700

    Se agrego la cuenta atras y el boton de reset

--- Temporizador/frmRegresiva.Designer.cs
+++ Temporizador/frmRegresiva.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             label5 = new Label();
             label4 = new Label();
             lblSegundos = new Label();
@@ -35,15 +36,18 @@
             lblHoras = new Label();
             btnEstablecer = new Button();
             button2 = new Button();
+            btnReset = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
             SuspendLayout();
             // 
             // label5
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.Location = new Point(489, 14);
+            label5.Location = new Point(342, 8);
+            label5.Margin = new Padding(2, 0, 2, 0);
             label5.Name = "label5";
-            label5.Size = new Size(111, 191);
+            label5.Size = new Size(75, 128);
             label5.TabIndex = 11;
             label5.Text = ":";
             // 
@@ -51,9 +55,10 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.Location = new Point(201, 14);
+            label4.Location = new Point(141, 8);
+            label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new Size(111, 191);
+            label4.Size = new Size(75, 128);
             label4.TabIndex = 10;
             label4.Text = ":";
             // 
@@ -61,9 +66,10 @@
             // 
             lblSegundos.AutoSize = true;
             lblSegundos.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblSegundos.Location = new Point(562, 27);
+            lblSegundos.Location = new Point(393, 16);
+            lblSegundos.Margin = new Padding(2, 0, 2, 0);
             lblSegundos.Name = "lblSegundos";
-            lblSegundos.Size = new Size(236, 191);
+            lblSegundos.Size = new Size(158, 128);
             lblSegundos.TabIndex = 9;
             lblSegundos.Text = "00";
             // 
@@ -71,9 +77,10 @@
             // 
             lblMinutos.AutoSize = true;
             lblMinutos.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblMinutos.Location = new Point(296, 27);
+            lblMinutos.Location = new Point(207, 16);
+            lblMinutos.Margin = new Padding(2, 0, 2, 0);
             lblMinutos.Name = "lblMinutos";
-            lblMinutos.Size = new Size(236, 191);
+            lblMinutos.Size = new Size(158, 128);
             lblMinutos.TabIndex = 8;
             lblMinutos.Text = "00";
             // 
@@ -81,17 +88,19 @@
             // 
             lblHoras.AutoSize = true;
             lblHoras.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblHoras.Location = new Point(-1, 27);
+            lblHoras.Location = new Point(-1, 16);
+            lblHoras.Margin = new Padding(2, 0, 2, 0);
             lblHoras.Name = "lblHoras";
-            lblHoras.Size = new Size(236, 191);
+            lblHoras.Size = new Size(158, 128);
             lblHoras.TabIndex = 7;
             lblHoras.Text = "00";
             // 
             // btnEstablecer
             // 
-            btnEstablecer.Location = new Point(281, 230);
+            btnEstablecer.Location = new Point(138, 138);
+            btnEstablecer.Margin = new Padding(2);
             btnEstablecer.Name = "btnEstablecer";
-            btnEstablecer.Size = new Size(112, 69);
+            btnEstablecer.Size = new Size(78, 41);
             btnEstablecer.TabIndex = 12;
             btnEstablecer.Text = "Establecer Tiempo";
             btnEstablecer.UseVisualStyleBackColor = true;
@@ -99,18 +108,37 @@
             // 
             // button2
             // 
-            button2.Location = new Point(405, 230);
+            button2.Location = new Point(254, 138);
+            button2.Margin = new Padding(2);
             button2.Name = "button2";
-            button2.Size = new Size(112, 69);
+            button2.Size = new Size(78, 41);
             button2.TabIndex = 13;
             button2.Text = "Iniciar";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // btnReset
+            // 
+            btnReset.Location = new Point(366, 139);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(75, 40);
+            btnReset.TabIndex = 14;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = true;
+            btnReset.Click += btnReset_Click;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
             // 
             // frmRegresiva
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 324);
+            ClientSize = new Size(560, 194);
+            Controls.Add(btnReset);
             Controls.Add(button2);
             Controls.Add(btnEstablecer);
             Controls.Add(label5);
@@ -118,6 +146,7 @@
             Controls.Add(lblSegundos);
             Controls.Add(lblMinutos);
             Controls.Add(lblHoras);
+            Margin = new Padding(2);
             Name = "frmRegresiva";
             Text = "Cuenta Regresiva";
             ResumeLayout(false);
@@ -133,5 +162,7 @@
         private Label lblHoras;
         private Button btnEstablecer;
         private Button button2;
+        private Button btnReset;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Temporizador/frmRegresiva.cs
+++ Temporizador/frmRegresiva.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,11 +13,15 @@ namespace Temporizador
 {
     public partial class frmRegresiva : Form
     {
-
+        int seg;
+        int min;
+        int h;
 
         public frmRegresiva()
         {
             InitializeComponent();
+
+            
         }
 
         private void btnEstablecer_Click(object sender, EventArgs e)
@@ -28,9 +33,16 @@ namespace Temporizador
             if (resultado == DialogResult.OK)
             {
                 lblSegundos.Text = formatear(establecer.tiempo[0].ToString());
-                lblMinutos.Text = formatear( establecer.tiempo[1].ToString());
-                lblHoras.Text =formatear( establecer.tiempo[2].ToString());
+                lblMinutos.Text = formatear(establecer.tiempo[1].ToString());
+                lblHoras.Text = formatear(establecer.tiempo[2].ToString());
+
+                h = establecer.tiempo[2];
+                min = establecer.tiempo[1];
+                seg = establecer.tiempo[0];
+
+                
             }
+            
         }
 
         private string formatear(string numero)
@@ -40,6 +52,50 @@ namespace Temporizador
                 return "0" + numero;
             }
             return numero;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            seg = 0;
+            min = 0;
+            h = 0; 
+            timer1.Stop();
+            lblSegundos.Text = "00";
+            lblMinutos.Text = "00";
+            lblHoras.Text = "00";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (seg > 0)
+            {
+                seg--;
+            }
+            else
+            {
+                if (min > 0)
+                {
+                    min--;
+                    seg = 59;
+                } else if (h > 0)
+                {
+                    h--;
+                    min = 59;
+                    seg = 59;
+                }
+                if (h == 0 && min == 0 && seg == 0)
+                {
+                    timer1.Stop();
+                }
+            }
+            lblSegundos.Text = formatear(seg.ToString());
+            lblMinutos.Text = formatear(min.ToString());
+            lblHoras.Text = formatear(h.ToString());
         }
     }
 }

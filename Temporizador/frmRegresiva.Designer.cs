f093061374da284b9ee651cf361bbf4c07e17438
danielaaldaco <danielaaldaco9@gmail.com>
2024-09-27 23:00:34 -0600

    Se codificaron los botones iniciar, donde retrocedia el temporizador hasta llegar a cero totalmente despues de haber establecido el tiempo, ademas se agrego un boton de camcelar para detener el temporizador si es que el usuario lo desea

--- Temporizador/frmRegresiva.Designer.cs
+++ Temporizador/frmRegresiva.Designer.cs
@@ -28,22 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             label5 = new Label();
             label4 = new Label();
             lblSegundos = new Label();
             lblMinutos = new Label();
             lblHoras = new Label();
             btnEstablecer = new Button();
-            button2 = new Button();
+            btnControl = new Button();
+            btnCancelar = new Button();
+            tmrRegresiva = new System.Windows.Forms.Timer(components);
             SuspendLayout();
             // 
             // label5
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label5.Location = new Point(489, 14);
+            label5.Location = new Point(342, 8);
+            label5.Margin = new Padding(2, 0, 2, 0);
             label5.Name = "label5";
-            label5.Size = new Size(111, 191);
+            label5.Size = new Size(75, 128);
             label5.TabIndex = 11;
             label5.Text = ":";
             // 
@@ -51,9 +55,10 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.Location = new Point(201, 14);
+            label4.Location = new Point(141, 8);
+            label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new Size(111, 191);
+            label4.Size = new Size(75, 128);
             label4.TabIndex = 10;
             label4.Text = ":";
             // 
@@ -61,9 +66,10 @@
             // 
             lblSegundos.AutoSize = true;
             lblSegundos.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblSegundos.Location = new Point(562, 27);
+            lblSegundos.Location = new Point(393, 16);
+            lblSegundos.Margin = new Padding(2, 0, 2, 0);
             lblSegundos.Name = "lblSegundos";
-            lblSegundos.Size = new Size(236, 191);
+            lblSegundos.Size = new Size(158, 128);
             lblSegundos.TabIndex = 9;
             lblSegundos.Text = "00";
             // 
@@ -71,9 +77,10 @@
             // 
             lblMinutos.AutoSize = true;
             lblMinutos.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblMinutos.Location = new Point(296, 27);
+            lblMinutos.Location = new Point(207, 16);
+            lblMinutos.Margin = new Padding(2, 0, 2, 0);
             lblMinutos.Name = "lblMinutos";
-            lblMinutos.Size = new Size(236, 191);
+            lblMinutos.Size = new Size(158, 128);
             lblMinutos.TabIndex = 8;
             lblMinutos.Text = "00";
             // 
@@ -81,43 +88,65 @@
             // 
             lblHoras.AutoSize = true;
             lblHoras.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblHoras.Location = new Point(-1, 27);
+            lblHoras.Location = new Point(-1, 16);
+            lblHoras.Margin = new Padding(2, 0, 2, 0);
             lblHoras.Name = "lblHoras";
-            lblHoras.Size = new Size(236, 191);
+            lblHoras.Size = new Size(158, 128);
             lblHoras.TabIndex = 7;
             lblHoras.Text = "00";
             // 
             // btnEstablecer
             // 
-            btnEstablecer.Location = new Point(281, 230);
+            btnEstablecer.Location = new Point(229, 138);
+            btnEstablecer.Margin = new Padding(2);
             btnEstablecer.Name = "btnEstablecer";
-            btnEstablecer.Size = new Size(112, 69);
+            btnEstablecer.Size = new Size(78, 41);
             btnEstablecer.TabIndex = 12;
             btnEstablecer.Text = "Establecer Tiempo";
             btnEstablecer.UseVisualStyleBackColor = true;
             btnEstablecer.Click += btnEstablecer_Click;
             // 
-            // button2
+            // btnControl
             // 
-            button2.Location = new Point(405, 230);
-            button2.Name = "button2";
-            button2.Size = new Size(112, 69);
-            button2.TabIndex = 13;
-            button2.Text = "Iniciar";
-            button2.UseVisualStyleBackColor = true;
+            btnControl.Location = new Point(311, 138);
+            btnControl.Margin = new Padding(2);
+            btnControl.Name = "btnControl";
+            btnControl.Size = new Size(78, 41);
+            btnControl.TabIndex = 13;
+            btnControl.Text = "Iniciar";
+            btnControl.UseVisualStyleBackColor = true;
+            btnControl.Click += button2_Click;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(147, 138);
+            btnCancelar.Margin = new Padding(2);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(78, 41);
+            btnCancelar.TabIndex = 14;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.Click += btnCancelar_Click;
+            // 
+            // tmrRegresiva
+            // 
+            tmrRegresiva.Interval = 200;
+            tmrRegresiva.Tick += tmrRegresiva_Tick;
             // 
             // frmRegresiva
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 324);
-            Controls.Add(button2);
+            ClientSize = new Size(560, 194);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnControl);
             Controls.Add(btnEstablecer);
             Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(lblSegundos);
             Controls.Add(lblMinutos);
             Controls.Add(lblHoras);
+            Margin = new Padding(2);
             Name = "frmRegresiva";
             Text = "Cuenta Regresiva";
             ResumeLayout(false);
@@ -132,6 +161,8 @@
         private Label lblMinutos;
         private Label lblHoras;
         private Button btnEstablecer;
-        private Button button2;
+        private Button btnControl;
+        private Button btnCancelar;
+        private System.Windows.Forms.Timer tmrRegresiva;
     }
 }
--- Temporizador/frmRegresiva.cs
+++ Temporizador/frmRegresiva.cs
@@ -18,9 +18,14 @@ namespace Temporizador
         {
             InitializeComponent();
         }
+        int segundo, minuto, hora;
+        bool bandera = false;
 
         private void btnEstablecer_Click(object sender, EventArgs e)
         {
+            lblSegundos.Text = "00";
+            lblMinutos.Text = "00";
+            lblHoras.Text = "00";
 
             DialogResult resultado;
             frmEstablecer establecer = new frmEstablecer();
@@ -28,8 +33,12 @@ namespace Temporizador
             if (resultado == DialogResult.OK)
             {
                 lblSegundos.Text = formatear(establecer.tiempo[0].ToString());
-                lblMinutos.Text = formatear( establecer.tiempo[1].ToString());
-                lblHoras.Text =formatear( establecer.tiempo[2].ToString());
+                lblMinutos.Text = formatear(establecer.tiempo[1].ToString());
+                lblHoras.Text = formatear(establecer.tiempo[2].ToString());
+
+                segundo = establecer.tiempo[0];
+                minuto = establecer.tiempo[1];
+                hora = establecer.tiempo[2];
             }
         }
 
@@ -40,6 +49,73 @@ namespace Temporizador
                 return "0" + numero;
             }
             return numero;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (bandera == false)
+            {
+                bandera = true;
+                tmrRegresiva.Enabled = true;
+                btnControl.Text = "Pausar";
+            }
+            else
+            {
+                bandera = false;
+                tmrRegresiva.Enabled = false;
+                btnControl.Text = "Iniciar";
+            }
+
+        }
+
+        private void tmrRegresiva_Tick(object sender, EventArgs e)
+        {
+            segundo--;
+
+            if (segundo < 0)
+            {
+                segundo = 59;
+                minuto--;
+
+                if (minuto < 0)
+                {
+                    minuto = 59;
+                    hora--;
+
+                }
+            }
+
+            if (segundo < 10)
+                lblSegundos.Text = "0" + segundo.ToString();
+            else
+                lblSegundos.Text = segundo.ToString();
+
+            if (minuto < 10)
+                lblMinutos.Text = "0" + minuto.ToString();
+            else
+                lblMinutos.Text = minuto.ToString();
+
+            if (hora < 10)
+                lblHoras.Text = "0" + hora.ToString();
+            else
+                lblHoras.Text = hora.ToString();
+
+
+            if (hora == 0 && minuto == 0 && segundo == 0)
+            {
+                tmrRegresiva.Stop();
+                MessageBox.Show("Tiempo terminado");
+            }
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            tmrRegresiva.Enabled = false;
+            btnControl.Text = "Iniciar";
+            lblSegundos.Text = "00";
+            lblMinutos.Text = "00";
+            lblHoras.Text = "00";
         }
     }
 }

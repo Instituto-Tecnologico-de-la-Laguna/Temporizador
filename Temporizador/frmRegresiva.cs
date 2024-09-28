3c37f550cbbdc0bf672c593d117a58bebc9e7942
vanne <alu.23130017@correo.itlalaguna.edu.mx>
2024-09-28 00:26:14 -0600

    se ha creado el frm Regresivo

--- Temporizador/frmRegresiva.cs
+++ Temporizador/frmRegresiva.cs
@@ -18,7 +18,11 @@ namespace Temporizador
         {
             InitializeComponent();
         }
-
+        int segundo;
+        int minuto;
+        int hora;
+        bool bandera = false;
+        frmEstablecer establecer = new frmEstablecer();
         private void btnEstablecer_Click(object sender, EventArgs e)
         {
 
@@ -28,8 +32,8 @@ namespace Temporizador
             if (resultado == DialogResult.OK)
             {
                 lblSegundos.Text = formatear(establecer.tiempo[0].ToString());
-                lblMinutos.Text = formatear( establecer.tiempo[1].ToString());
-                lblHoras.Text =formatear( establecer.tiempo[2].ToString());
+                lblMinutos.Text = formatear(establecer.tiempo[1].ToString());
+                lblHoras.Text = formatear(establecer.tiempo[2].ToString());
             }
         }
 
@@ -40,6 +44,48 @@ namespace Temporizador
                 return "0" + numero;
             }
             return numero;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int segundo = 0, minuto = 1, hora = 2;
+
+            if (minuto < 0)
+            {
+                hora--;
+                minuto = 59;
+                hora = establecer.tiempo[2];
+            }
+            if (segundo < 0)
+            {
+                minuto--;
+                segundo = 59;
+                segundo = establecer.tiempo[0];
+            }
+            
+            
+            if (segundo < 10)
+                lblSegundos.Text = "0" + segundo.ToString();
+            else
+                lblSegundos.Text = segundo.ToString();
+            
+            if (hora < 10)
+                lblHoras.Text = "0" + hora.ToString();
+            else
+                lblHoras.Text = hora.ToString();
+            
+            if(minuto < 10)
+                lblMinutos.Text = "0" + minuto.ToString();
+            else
+                lblMinutos.Text = minuto.ToString();
+            
+
+            if(hora == 0 && minuto == 0 && segundo == 0)
+            {
+                return;
+            }
+
+            segundo--;
         }
     }
 }
